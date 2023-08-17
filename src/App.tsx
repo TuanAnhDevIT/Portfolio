ddc21d8ba3c7b3c3c95d4a6cd21dfe017bcdc913
DangTranTuanAnh <dangtrantuananh.2002@gmail.com>
2023-08-17 11:45:50 +0700

    chia layout portfolio

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,8 @@
-import { useState } from 'react'
-import './App.css'
-import Video10 from './example/video10/video10';
-
+import Portfolio from './assets/screens/portfolio'
 
 function App() {
   return (
-    <div className="App">
-      <p className="read-the-docs">
-        <Video10 defaultAge={""} defaultName={""} />
-      </p>
-    </div>
+    <Portfolio />
   )
 }
 
